667b2c197ee6bd34625d779a8bdd55dfda3732fc
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2021-09-13 13:41:49 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio4/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Produto
+    {
+        //variaveis 
+            public string Nome;
+            public double Preco;
+            public int Quantidade;
+
+       
+            
+        // metodo valor total do estoque
+            public double ValorTotalEmEstoque() { 
+            return Preco * Quantidade;
+        }
+
+        //metodo adicionar produto
+        public void AdicionarProduto(int quantidade) { 
+            Quantidade += quantidade; 
+        }
+
+
+        //metodo remover produto
+        public void RemoverProduto(int quantidade) { 
+            Quantidade = Quantidade - quantidade; 
+        }
+
+        public override string ToString() {
+            return Nome 
+                + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " 
+                + Quantidade
+                + " unidades, Total: R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // esta instanciando
+            Produto p = new Produto(); 
+
+            // entrada dos dados do produto do usuario
+            Console.WriteLine("Entre com os dados do produto;"); 
+
+            // nome do produto
+            Console.Write("Nome: "); 
+            p.Nome = Console.ReadLine(); 
+
+            //preço do produto
+            Console.Write("Preço: "); 
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // quantidade de produto no estoque
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            
+            //dados do produto
+            Console.WriteLine("Dados do produto: " + p); 
+
+            //adicionar produto no estoque
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: "); 
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte); 
+
+            Console.WriteLine("Dados do atualizados: " + p);
+
+            // remover produto do estoque 
+            Console.WriteLine("Digite o número de produtos a serem removidos do estoque: "); 
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte); 
+
+            Console.WriteLine("Dados do atualizados: " + p);
+        }
+    }
+}
